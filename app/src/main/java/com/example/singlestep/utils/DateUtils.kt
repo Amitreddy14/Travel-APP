8f5a0641c4867d0876225a2ea20b3244017352ef
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-26 23:44:08 +0530

    getSelectedDateRange function to DateUtils for date range formatting

--- app/src/main/java/com/example/singlestep/utils/DateUtils.kt
+++ app/src/main/java/com/example/singlestep/utils/DateUtils.kt
@@ -9,3 +9,13 @@ import java.time.temporal.ChronoUnit
 import java.util.Calendar
 import java.util.Locale
 import java.util.TimeZone
+
+fun getSelectedDateRange(selection: Pair<Long, Long>): String {
+    val utc = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+    utc.timeInMillis = selection.first
+    val startDateString = getDateString(utc)
+    utc.timeInMillis = selection.second
+    val endDateString = getDateString(utc)
+    return "$startDateString - $endDateString"
+}
+
