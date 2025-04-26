c6b5b72ac687910e9919909e1e92dc42ed9e57bc
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-27 00:07:28 +0530

    ViewUtils: initialize utility functions for handling views and places

---
+++ app/src/main/java/com/example/singlestep/utils/ViewUtils.kt
@@ -0,0 +1,17 @@
+package com.example.singlestep.utils
+
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.example.singlestep.R
+import com.google.android.gms.common.api.Status
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.model.PlaceTypes
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
+import com.google.android.material.bottomnavigation.BottomNavigationView
