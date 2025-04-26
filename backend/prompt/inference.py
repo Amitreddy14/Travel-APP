da95b19b2872eb13f5b3ba3bc83f50d942c059c5
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-26 23:32:05 +0530

    inference module to initialize OpenAI client and enable debugging

--- backend/prompt/inference.py
+++ backend/prompt/inference.py
@@ -10,3 +10,19 @@ MODEL_3_5 = "gpt-3.5-turbo-0125"
 MODEL_4 = "gpt-4-0125-preview"
 
 MODEL = MODEL_3_5
+
+# Flag to turn on debugging prints for GPT input and output
+debug = True
+
+# load in API key from .env
+load_dotenv()
+
+openai_key = os.environ.get("OPENAI_KEY")
+oai_client = OpenAI(api_key=openai_key)
+
+# Enables `response_model`
+client = instructor.patch(oai_client, mode=instructor.Mode.JSON)
+
+
+def get_client():
+    return client
