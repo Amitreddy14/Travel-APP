e44c65cffdac290b60f4b2a06b71b44ad3239ed9
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-26 23:46:50 +0530

    getBoundingBox function to LocationUtils for bounding box calculations

--- app/src/main/java/com/example/singlestep/utils/LocationUtils.kt
+++ app/src/main/java/com/example/singlestep/utils/LocationUtils.kt
@@ -6,3 +6,30 @@ import kotlin.math.cos
 import kotlin.math.max
 import kotlin.math.min
 import kotlin.math.sin
+
+fun getBoundingBox(centerPoint: Pair<Double, Double>, distance: Double): List<Double> {
+    val minLatLimit: Double
+    val maxLatLimit: Double
+    val minLonLimit: Double
+    val maxLonLimit: Double
+    val radLat: Double
+    val radLon: Double
+    var minLat: Double
+    var maxLat: Double
+    var minLon: Double
+    var maxLon: Double
+    val deltaLon: Double
+
+    if (distance < 0) {
+        return listOf() // Returning an empty list as an indication of illegal arguments
+    }
+
+    // Helper functions (degrees to radians)
+    fun Number.degToRad(): Double = this.toDouble() * (Math.PI / 180)
+    fun Number.radToDeg(): Double = this.toDouble() * (180 / Math.PI)
+
+    // Coordinate limits
+    minLatLimit = (-90).degToRad()
+    maxLatLimit = (90).degToRad()
+    minLonLimit = (-180).degToRad()
+    maxLonLimit = (180).degToRad()
