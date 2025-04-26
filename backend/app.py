a9dec4c19b0e3ee57179e3b3b941d73052e7b3f2
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-26 23:23:34 +0530

    itinerary generation freeform endpoint with parameter validation

--- backend/app.py
+++ backend/app.py
@@ -40,3 +40,26 @@ def generate_itinerary():
 
 @cross_origin()
 @app.route("/generate-itinerary-freeform", methods=["POST"])
+def generate_itinerary_freeform():
+    trip_location = request.json["trip_location"]
+    arrival_time = request.json["arrival_time"]
+    departure_time = request.json["departure_time"]
+    hotel_address = request.json["hotel_address"]
+    total_budget = request.json["total_budget"]
+
+    # No parameter should be None
+    if any(
+        var is None
+        for var in [arrival_time, departure_time, trip_location, total_budget]
+    ):
+        return Response("Validation error: Missing required parameters", status=400)
+
+    output = get_itinerary_freeform(
+        trip_location, arrival_time, departure_time, hotel_address, total_budget
+    )
+
+    return output
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8080)
