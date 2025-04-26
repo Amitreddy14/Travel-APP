f56a6e77c2f7d9898c422dfb82f5fe844c418675
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-26 23:45:52 +0530

    LocationUtils utility class for location-related calculations

---
+++ app/src/main/java/com/example/singlestep/utils/LocationUtils.kt
@@ -0,0 +1,8 @@
+package com.example.singlestep.utils
+
+import kotlin.math.PI
+import kotlin.math.asin
+import kotlin.math.cos
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.math.sin
