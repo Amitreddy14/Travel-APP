cacf6b5fe15b91320ed63bf63f101f9e51310c7e
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-26 23:39:19 +0530

    dependencies for core libraries and RoomDB integration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,3 +43,30 @@ android {
         viewBinding = true
     }
 }
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("com.google.android.libraries.places:places:3.3.0")
+    implementation("com.amadeus:amadeus-android:1.3.1")
+    implementation("org.jsoup:jsoup:1.11.2")
+    implementation("com.facebook.shimmer:shimmer:0.5.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:converter-scalars:2.10.0")
+    implementation("com.google.android.gms:play-services-location:18.0.0")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:1.1.5")
+
+    // RoomDB
+    val room_version = "2.6.1"
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+    kapt("androidx.room:room-compiler:$room_version")
+}
