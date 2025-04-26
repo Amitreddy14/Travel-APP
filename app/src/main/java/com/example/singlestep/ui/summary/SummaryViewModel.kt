d649c8f19f488cb98422cb980a93b2900c742415
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-27 00:24:04 +0530

    SummaryViewModel: refactor initialization and coroutine exception handling

--- app/src/main/java/com/example/singlestep/ui/summary/SummaryViewModel.kt
+++ app/src/main/java/com/example/singlestep/ui/summary/SummaryViewModel.kt
@@ -15,3 +15,26 @@ import com.example.singlestep.utils.Result
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+
+class SummaryViewModel(
+    application: Application,
+    savedStateHandle: SavedStateHandle,
+) : AndroidViewModel(application) {
+
+    private var coroutineExceptionHandler: CoroutineExceptionHandler
+
+    private val assistant = Assistant()
+    private var dao = AppDatabase.getDatabase(application.applicationContext).tripSummaryDao()
+
+    private val tripSummary =
+        savedStateHandle.getLiveData<TripSummary>("tripSummary").value!!
+
+    private val _itineraryString: MutableLiveData<Result<String>> = MutableLiveData()
+    val itineraryString: LiveData<Result<String>>
+        get() = _itineraryString
+
+    init {
+        coroutineExceptionHandler = CoroutineExceptionHandler { _, exception ->
+            _itineraryString.value = Result.Failure(exception)
+        }
+    }
