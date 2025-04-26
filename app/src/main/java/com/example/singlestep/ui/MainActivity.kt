859f1d59799f4cdd942bc13a5df2c6308fba5e5b
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-27 00:12:48 +0530

    MainActivity with essential imports and package declaration

---
+++ app/src/main/java/com/example/singlestep/ui/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.example.singlestep.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.singlestep.R
+import com.example.singlestep.databinding.ActivityMainBinding
+import com.google.android.libraries.places.api.Places
