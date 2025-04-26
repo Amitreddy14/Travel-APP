d26745f382db87e6a16c9f0ad267bd18808ab193
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-27 00:13:35 +0530

    streamline onCreate method and ensure Places API initialization

--- app/src/main/java/com/example/singlestep/ui/MainActivity.kt
+++ app/src/main/java/com/example/singlestep/ui/MainActivity.kt
@@ -8,3 +8,22 @@ import androidx.navigation.ui.setupWithNavController
 import com.example.singlestep.R
 import com.example.singlestep.databinding.ActivityMainBinding
 import com.google.android.libraries.places.api.Places
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        setContentView(viewBinding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        viewBinding.bottomNavigationView.setupWithNavController(navController)
+
+        val apiKey = getString(R.string.places_api_key)
+        if (!Places.isInitialized()) {
+            Places.initialize(applicationContext, apiKey)
+        }
+    }
+}
